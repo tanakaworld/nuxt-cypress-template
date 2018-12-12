909800e74ae2000e384330ead5b5b9479c3457ff
tanakaworld <yutaro.tanaka.world@gmail.com>
2018-12-12 13:33:55 +0900

    create nuxt-app

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # nuxt-cypress-template
+
+> Template for Nuxt + Cypress + TypeScript
+
 Nuxt + Cypress https://www.cypress.io/
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ yarn install
+
+# serve with hot reload at localhost:3000
+$ yarn run dev
+
+# build for production and launch server
+$ yarn run build
+$ yarn start
+
+# generate static project
+$ yarn run generate
+```
+
+For detailed explanation on how things work, checkout [Nuxt.js docs](https://nuxtjs.org).
---
+++ nuxt.config.js
@@ -0,0 +1,32 @@
+const pkg = require('./package')
+
+module.exports = {
+  mode: 'universal',
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+  loading: { color: '#fff' },
+  css: [],
+  plugins: [],
+  modules: ['@nuxtjs/axios'],
+  axios: {},
+  build: {
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
